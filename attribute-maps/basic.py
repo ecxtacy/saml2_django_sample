d1d674c79a9069fea55c8c0fb745d7284259553e
ecxtacy <dc.dhruvchouhan@gmail.com>
2024-06-17 17:29:57 +0530

    sample of saml2 django

---
+++ attribute-maps/basic.py
@@ -0,0 +1,6 @@
+MAP = {
+    'urn:mace:dir:attribute-def:uid': 'uid',
+    'urn:mace:dir:attribute-def:mail': 'email',
+    'urn:mace:dir:attribute-def:sn': 'surname',
+    'urn:mace:dir:attribute-def:givenName': 'first_name',
+}
